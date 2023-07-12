3c5413de5db0b142483ee53a67a59aa29e6a1f64
Stuart Cooper <stuart@goldsmidhouse.com>
2023-07-12 22:01:28 +0100

    fix typos

--- app/models/season.rb
+++ app/models/season.rb
@@ -24,7 +24,7 @@ class Season < ApplicationRecord
   belongs_to :league
   has_many :fixtures
   has_many :team_seasons
-  has_many :player_seasons
+  # has_many :player_seasons
   
   def next_round_of_fixtures
     fixtures.where(season_id: id, game_week: game_week + 1)
