97e4f7dfe10c564237e1dc92a043582f6a275fbe
repoocuts <stuart.c@meetcleo.com>
2023-11-01 19:16:35 +0000

    fixed dashboard and screen size adjustment thanks to tailwind.config.js values

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,76 +1,77 @@
 class LeaguesController < ApplicationController
-  before_action :set_league, only: %i[ show edit update destroy ]
-  before_action :set_season, only: %i[ index show edit update destroy ]
+	before_action :set_league, only: %i[ show edit update destroy ]
+	before_action :set_season, only: %i[ index show edit update destroy ]
 
-  # GET /leagues or /leagues.json
-  def index
-    @league = TeamSeason.where(season: season).all.order(:points).reverse
-  end
+	# GET /leagues or /leagues.json
+	def index
+		@league = season.team_seasons.includes(:team).order(:points).reverse
+	end
 
-  # GET /leagues/1 or /leagues/1.json
-  def show
-  end
+	# GET /leagues/1 or /leagues/1.json
+	def show
+	end
 
-  # GET /leagues/new
-  def new
-    @league = League.new
-  end
+	# GET /leagues/new
+	def new
+		@league = League.new
+	end
 
-  # GET /leagues/1/edit
-  def edit
-  end
+	# GET /leagues/1/edit
+	def edit
+	end
 
-  # POST /leagues or /leagues.json
-  def create
-    @league = League.new(league_params)
+	# POST /leagues or /leagues.json
+	def create
+		@league = League.new(league_params)
 
-    respond_to do |format|
-      if @league.save
-        format.html { redirect_to league_url(@league), notice: "League was successfully created." }
-        format.json { render :show, status: :created, location: @league }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @league.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @league.save
+				format.html { redirect_to league_url(@league), notice: "League was successfully created." }
+				format.json { render :show, status: :created, location: @league }
+			else
+				format.html { render :new, status: :unprocessable_entity }
+				format.json { render json: @league.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /leagues/1 or /leagues/1.json
-  def update
-    respond_to do |format|
-      if @league.update(league_params)
-        format.html { redirect_to league_url(@league), notice: "League was successfully updated." }
-        format.json { render :show, status: :ok, location: @league }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @league.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /leagues/1 or /leagues/1.json
+	def update
+		respond_to do |format|
+			if @league.update(league_params)
+				format.html { redirect_to league_url(@league), notice: "League was successfully updated." }
+				format.json { render :show, status: :ok, location: @league }
+			else
+				format.html { render :edit, status: :unprocessable_entity }
+				format.json { render json: @league.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /leagues/1 or /leagues/1.json
-  def destroy
-    @league.destroy
+	# DELETE /leagues/1 or /leagues/1.json
+	def destroy
+		@league.destroy
 
-    respond_to do |format|
-      format.html { redirect_to leagues_url, notice: "League was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to leagues_url, notice: "League was successfully destroyed." }
+			format.json { head :no_content }
+		end
+	end
 
-  private
-    attr_reader :league, :season
-    # Use callbacks to share common setup or constraints between actions.
-    def set_league
-      @league = League.find(params[:id])
-    end
+	private
 
-    def set_season
-      @season = Season.current_season
-    end
+	attr_reader :league, :season
+	# Use callbacks to share common setup or constraints between actions.
+	def set_league
+		@league = League.find(params[:id])
+	end
 
-    # Only allow a list of trusted parameters through.
-    def league_params
-      params.require(:league).permit(:name, :api_football_id, :country_id)
-    end
+	def set_season
+		@season = Season.current_season
+	end
+
+	# Only allow a list of trusted parameters through.
+	def league_params
+		params.require(:league).permit(:name, :api_football_id, :country_id)
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  include Pagy::Frontend
+	include Pagy::Frontend
 
-  def convert_datetime(time)
-    time.strftime("%a %e %b %H:%M")
-  end
+	def convert_datetime_long(time)
+		time.strftime("%a %e %b %H:%M")
+	end
+
+	def convert_datetime_short(time)
+		time.strftime("%d/%m/%y")
+	end
+
 end
