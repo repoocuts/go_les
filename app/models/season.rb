55299a395c02ce535d3be2d7103bd04ded6dd82a
repoocuts <stuart.c@meetcleo.com>
2023-11-10 20:53:07 +0000

    remove other cache

--- app/models/season.rb
+++ app/models/season.rb
@@ -59,27 +59,21 @@ class Season < ApplicationRecord
 	end
 
 	def top_scorers
-		Rails.cache.fetch("top_scorers", expires_in: 12.hours) do
-			grouped_goals = goals.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
-			sorted_counts = grouped_goals.map { |player_season, goals| [player_season, goals.count] }
-			sorted_counts.sort_by! { |_, count| -count }
-		end
+		grouped_goals = goals.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
+		sorted_counts = grouped_goals.map { |player_season, goals| [player_season, goals.count] }
+		sorted_counts.sort_by! { |_, count| -count }
 	end
 
 	def top_assists
-		Rails.cache.fetch("top_assists", expires_in: 12.hours) do
-			grouped_assists = assists.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
-			sorted_counts = grouped_assists.map { |player_season, assist| [player_season, assist.count] }
-			sorted_counts.sort_by! { |_, count| -count }
-		end
+		grouped_assists = assists.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
+		sorted_counts = grouped_assists.map { |player_season, assist| [player_season, assist.count] }
+		sorted_counts.sort_by! { |_, count| -count }
 	end
 
 	def top_booked
-		Rails.cache.fetch("top_booked", expires_in: 12.hours) do
-			grouped_cards = cards.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
-			sorted_counts = grouped_cards.map { |player_season, card| [player_season, card.count] }
-			sorted_counts.sort_by! { |_, count| -count }
-		end
+		grouped_cards = cards.includes(player_season: [:player, { team_season: :team }]).group_by(&:player_season)
+		sorted_counts = grouped_cards.map { |player_season, card| [player_season, card.count] }
+		sorted_counts.sort_by! { |_, count| -count }
 	end
 
 	private
