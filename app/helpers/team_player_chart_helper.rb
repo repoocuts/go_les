98a016a83133e718e6f80e4b809e63e27f523465
repoocuts <stuart.c@meetcleo.com>
2023-11-14 20:06:45 +0000

    added chart config

--- app/helpers/team_player_chart_helper.rb
+++ app/helpers/team_player_chart_helper.rb
@@ -3,28 +3,54 @@ module TeamPlayerChartHelper
 	def team_player_season_goals
 		column_chart team_player_season_goals_path(@team.current_team_season.id),
 		             colors: ['red'],
-		             ytitle: 'Goals',
-		             library: { title: { text: 'Top Scorers', align: 'center' }, yAxis: { title: { text: 'Goals' },
-		                                                                                  allowDecimals: false,
-		                                                                                  minTickInterval: 1
+		             library: {
+			             yAxis: {
+				             title: {
+					             text: 'Goals' },
+				             allowDecimals: false,
+				             minTickInterval: 1
 		             },
-		                        xAxis: {
-			                        labels: {
-				                        enabled: false
-			                        }
-		                        }
+			             xAxis: {
+				             labels: {
+					             enabled: false
+				             }
+			             }
 		             }
 	end
 
 	def team_player_season_assists
 		column_chart team_player_season_assists_path(@team.current_team_season.id),
 		             colors: ['black'],
-		             ytitle: 'Assists'
+		             library: {
+			             yAxis: {
+				             title: {
+					             text: 'Assists' },
+				             allowDecimals: false,
+				             minTickInterval: 1
+			             },
+			             xAxis: {
+				             labels: {
+					             enabled: false
+				             }
+			             }
+		             }
 	end
 
 	def team_player_season_yellow_cards
 		column_chart team_player_season_assists_path(@team.current_team_season.id),
 		             colors: ['yellow'],
-		             ytitle: 'Assists'
+		             library: {
+			             yAxis: {
+				             title: {
+					             text: 'Yellow Cards' },
+				             allowDecimals: false,
+				             minTickInterval: 1
+			             },
+			             xAxis: {
+				             labels: {
+					             enabled: false
+				             }
+			             }
+		             }
 	end
 end
