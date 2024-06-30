e0812c9cb6606df5e69d05fbde8969398e3c884c
repoocuts <84625989+repoocuts@users.noreply.github.com>
2024-06-30 16:16:30 +0100

    explicitly call asset build for Tailwind

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.turbo.min.js", preload: true
-pin "tailwindcss", to: 'app/assets/stylesheets/application.tailwind.css', preload: true
 pin "chartkick", to: "chartkick.js", preload: true
 pin "Chart.bundle", to: "Chart.bundle.js", preload: true
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.0/dist/chart.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.debug = true
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( ceefax.js ceefax.css )
+Rails.application.config.assets.precompile += %w( application.tailwind.css )
